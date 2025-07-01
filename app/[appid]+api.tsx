4d0ab021c8d99b11ee4176352fb59fa291eb5f15
Brent Vatne <brentvatne@gmail.com>
2025-06-30 19:19:33 -0700

    Proxy headers

--- app/[appid]+api.tsx
+++ app/[appid]+api.tsx
@@ -3,13 +3,32 @@ export async function GET(request: Request) {
   urlToFetch.host = `u.expo.dev`;
   urlToFetch.port = '';
   urlToFetch.protocol = 'https:';
-  return await fetch(urlToFetch, {
-    method: request.method,
-    headers: {
-      'accept': request.headers.get('accept') || '*/*',
-      'accept-encoding': request.headers.get('accept-encoding') || 'gzip, br',
-      'cache-control': 'no-cache',
-      host: `u.expo.dev`,
-    },
-  });
+
+  // copy all headers from the request if they begin with expo- or eas-, case insensitive
+  const updateRequestHeaders = Object.fromEntries(
+    Array.from(request.headers.entries())
+      .filter(([key]) =>
+        key.toLowerCase().startsWith('expo-') || key.toLowerCase().startsWith('eas-')
+      )
+  );
+
+  try {
+    const result = await fetch(urlToFetch, {
+      method: request.method,
+      headers: {
+        ...updateRequestHeaders,
+        'accept': request.headers.get('accept') || '*/*',
+        'accept-encoding': request.headers.get('accept-encoding') || 'gzip, br',
+        'cache-control': 'no-cache'
+      },
+    });
+
+    return new Response(result.body, {
+      status: result.status,
+      headers: result.headers,
+    });
+  } catch(e) {
+    console.error(e);
+    return new Response('Error', { status: 500 });
+  }
 }
