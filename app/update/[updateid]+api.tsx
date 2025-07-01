51ca787b10386630dee3b5f0f31550611eb8b8d6
Quinlan Jung <quin@expo.io>
2025-06-30 18:52:13 -0700

    Temporary Commit at 6/30/2025, 6:52:13 PM

--- app/update/[updateid]+api.tsx
+++ app/update/[updateid]+api.tsx
@@ -1,14 +1,31 @@
 export async function GET(request: Request) {
   const urlToFetch = new URL(request.url);
   urlToFetch.host = `u.expo.dev`;
-  urlToFetch.port = '';
-  urlToFetch.protocol = 'https:';
+  urlToFetch.port = "";
+  urlToFetch.protocol = "https:";
   return await fetch(urlToFetch, {
     method: request.method,
     headers: {
-      'accept': request.headers.get('accept') || '*/*',
-      'accept-encoding': request.headers.get('accept-encoding') || 'gzip, br',
-      'cache-control': 'no-cache',
+      ...(request.headers.get("expo-platform")
+        ? { "expo-platform": request.headers.get("expo-platform") as string }
+        : {}),
+      ...(request.headers.get("expo-channel-name")
+        ? {
+            "expo-channel-name": request.headers.get(
+              "expo-channel-name"
+            ) as string,
+          }
+        : {}),
+      ...(request.headers.get("expo-runtime-version")
+        ? {
+            "expo-runtime-version": request.headers.get(
+              "expo-runtime-version"
+            ) as string,
+          }
+        : {}),
+      accept: request.headers.get("accept") || "*/*",
+      "accept-encoding": request.headers.get("accept-encoding") || "gzip, br",
+      "cache-control": "no-cache",
       host: `u.expo.dev`,
     },
   });
